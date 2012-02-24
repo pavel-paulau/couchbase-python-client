fe46dbba7f6ec4902a495245ecb4b16da548d998
Sebastian Hubbard <sebastian@chango.com>
2012-02-24 12:03:53 -0500

    more cleanup

--- capsule_test/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-opaque = Extension('opaque',
-                   sources = ['opaque.c'])
-
-setup (name = 'Opaque',
-       version = '1.0',
-       description = 'Opaque objects test',
-       ext_modules = [opaque])
