2afbd730e14c43d6d89a7d6c6cff50a27d1655a2
Pavel.Paulau <pavel.paulau@gmail.com>
2012-11-07 21:33:21 -0800

    clean up couchbase client

--- pylibcb.py
+++ pylibcb.py
@@ -1,27 +1,18 @@
 import _pylibcb
 
-# aliasing these exceptions for convenience
-ConnectionFailure = _pylibcb.ConnectionFailure
-Failure = _pylibcb.Failure
-KeyExists = _pylibcb.KeyExists
-OutOfMemory = _pylibcb.OutOfMemory
-Timeout = _pylibcb.Timeout
 
 class Client(object):
-    def __init__(self, host='localhost', user=None, passwd=None, bucket=None, timeout=0):
-        if not user:
-            user = ''
-        if not passwd:
-            passwd = ''
-        if not bucket:
-            bucket = ''
 
-        self.instance = _pylibcb.open(host, user, passwd, bucket)
+    """Couchbase client"""
+
+    def __init__(self, host='localhost', user='', password='', bucket='',
+                 timeout=0):
+        self.instance = _pylibcb.open(host, user, password, bucket)
         self.timeout = int(timeout * 1000)
-        
+
     def get(self, key, timeout=None):
         if timeout:
-            return _pylibcb.get(self.instance, key, int(timeout * 1000))
+            return _pylibcb.get(self.instance, key, timeout * 1000)
         return _pylibcb.get(self.instance, key, self.timeout)
 
     def gat(self, key, new_expiry, timeout=None):
