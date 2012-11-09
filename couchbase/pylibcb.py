80fd17aa629c194026b7e7ab1637a9d33d892dcc
Pavel.Paulau <pavel.paulau@gmail.com>
2012-11-08 18:08:55 -0800

    add docstrings for every memcached operarion

--- couchbase/pylibcb.py
+++ couchbase/pylibcb.py
@@ -7,46 +7,89 @@ class Client(object):
 
     def __init__(self, host='localhost', user='', password='', bucket='',
                  timeout=0):
+        """Open connection to Couchbase server.
+
+        :param host: hostname of IP address
+        :param user: administrative username (is SASL bucked is used)
+        :param password: administrative password (is SASL bucked is used)
+        :param bucket: bucket name
+        :param timeout: optional timeout in milliseconds"""
         self.instance = _pylibcb.open(host, user, password, bucket)
         self.timeout = int(timeout * 1000)
 
     def get(self, key, timeout=0):
+        """Get a value by key.
+
+        :param key: key to search for
+        :param timeout: optional timeout in milliseconds"""
         timeout = int(timeout * 1000) or self.timeout
         return _pylibcb.get(self.instance, key, timeout)
 
-    def gat(self, key, new_expiry, timeout=0):
+    def gat(self, key, expiry, timeout=0):
+        """Get and touch.
+
+        :param key: key to search for
+        :param expiry: new expiration time
+        :param timeout: optional timeout in milliseconds"""
         timeout = int(timeout * 1000) or self.timeout
-        return _pylibcb.get(self.instance, key, timeout, new_expiry)
+        return _pylibcb.get(self.instance, key, timeout, expiry)
 
     def get_cas(self, key, timeout=0):
+        """GET with CAS.
+
+        :param key: key to search for
+        :param timeout: optional timeout in milliseconds"""
         timeout = int(timeout * 1000) or self.timeout
         return _pylibcb.get(self.instance, key, timeout, 0, 1)
 
-    def gat_cas(self, key, new_expiry, timeout=0):
+    def gat_cas(self, key, expiry, timeout=0):
+        """GAT with CAS.
+
+        :param key: key to search for
+        :param expiry: new expiration time
+        :param timeout: optional timeout in milliseconds"""
         timeout = int(timeout * 1000) or self.timeout
-        return _pylibcb.get(self.instance, key, timeout, new_expiry, 1)
+        return _pylibcb.get(self.instance, key, timeout, expiry, 1)
 
     def set(self, key, value, expiry=0, cas=0):
+        """Set a value by key.
+
+        :param key: document key
+        :param value: document value
+        :param expiry: expiration time
+        :param cas: CAS (Compare And Swap) value"""
         return _pylibcb.set(self.instance, key, value, expiry, cas)
 
     def remove(self, key, cas=0):
+        """Remove a value by key
+
+        :param key: key of document to be removed
+        :param cas: CAS (Compare And Swap) value"""
         return _pylibcb.remove(self.instance, key)
 
     def get_async_limit(self):
+        """Get the limit for the number of requests allowed before one is
+        required to complete"""
         return _pylibcb.get_async_limit(self.instance)
 
     def set_async_limit(self, limit):
+        """Set the limit for the number of requests allowed before one is
+        required to complete"""
         return _pylibcb.set_async_limit(self.instance, limit)
 
     def get_async_count(self):
+        """Get the number of incomplete asynchronous requests waiting"""
         return _pylibcb.get_async_count(self.instance)
 
     def enable_async(self):
+        """Enable asynchronous behavior"""
         return _pylibcb.enable_async(self.instance)
 
     def disable_async(self):
+        """Disable asynchronous behavior"""
         return _pylibcb.disable_async(self.instance)
 
     def async_wait(self, timeout=0):
+        """Execute eventloop for a given number of milliseconds"""
         timeout = int(timeout * 1000) or self.timeout
         return _pylibcb.async_wait(self.instance, timeout)
